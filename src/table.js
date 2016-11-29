1d2783ce8ba19854f164d631da2cf58ca4bb0c5b
Sagar <sagar@zyoba.com>
2016-11-29 15:18:05 +0545

    show data in popup once user click to any row

--- src/table.js
+++ src/table.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import '../bower_components/bootstrap/dist/css/bootstrap.css'
-// import './bootstrap-theme.min.css'
+import Portal from 'react-portal';
 
 class Cell extends Component{
-	// constructor(props){
-	// 	super(props)
-	// }
+	constructor(props){
+		super(props)
+		this.cellClick = this.cellClick.bind(this);
+		this.titleChange = this.titleChange.bind(this);
+		this.state = {title : this.props.cellData.title}
 
+	}
+	cellClick(data){
+		// this.refs.myPortal.openPortal()
+		// alert(data)
+	}
+	titleChange(event){
+		this.setState({title: event.target.value});
+	}
   render(){
-  	return (
+  	const row = 
   			<tr>
-				  <td className="success">{this.props.cellData.id}</td>
-				  <td className="warning">{this.props.cellData.title}</td>
-				  <td className="warning">{this.props.cellData.userId}</td>
-
-		  	</tr>
+				  <td className="success"  onClick={() => this.cellClick(this.props.cellData.id)}>{this.props.cellData.id}</td>
+				  <td className="warning"  onClick={() => this.cellClick(this.props.cellData.title)}>{this.props.cellData.title}</td>
+				  <td className="warning" onClick={() => this.cellClick(this.props.cellData.userId)}>{this.props.cellData.userId}</td>
+			</tr>;
+  	return (
+  		 <Portal closeOnEsc closeOnOutsideClick openByClickOn={row}>
+			<PseudoModal>
+			<h2>Modal</h2>
+				<form>
+					<label> Title </label>
+					<input type="text" value={this.state.title} placeholder="Description" onChange={this.titleChange} />
+				</form>
+			<p></p>
+			</PseudoModal>
+      </Portal>
+  		
 	)
   }
 }
@@ -25,10 +46,6 @@ class Table extends Component{
 	// }
 	componentDidMount() {
 
-		// this.timerID = setInterval(
-		// () => this.tick(),
-		// 1000
-		// );
 	}
 
 	componentWillUnmount() {
@@ -43,16 +60,30 @@ class Table extends Component{
 
 		return (
 		<table className="table">
-			<tr>
-				<th>Firstname</th>
-				<th>Lastname</th> 
-				<th>Age</th>
-			</tr>
-			{cells}
+			<tbody>
+				<tr>
+					<th>ID</th>
+					<th>Title</th> 
+					<th>Userid</th>
+				</tr>
+				{cells}
+			</tbody>
   		</table>
 
 		);
 	}
+}
+export class PseudoModal extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <p><button onClick={this.props.closePortal}>Close this</button></p>
+      </div>
+    );
+  }
+
 }
 
 
